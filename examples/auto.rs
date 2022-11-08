4309d0715825a3cd315495ddaefb7bb7af19d4f2
arduano <leonid.shchurov@gmail.com>
2022-11-08 23:51:37 +1100

    Auto improvements

--- examples/auto.rs
+++ examples/auto.rs
@@ -21,13 +21,15 @@ fn main() {
 	eframe::run_native(
 		"Auto snake game",
 		options,
-		Box::new(|_cc| Box::new(MyApp::new(SnakeSpanningTreeSolver::new(Some(10))))),
+		Box::new(|_cc| Box::new(MyApp::new(SnakeSpanningTreeSolver::new(Some(1))))),
 	);
 }
 
 struct MyApp<SS: SnakeSolver> {
 	world: AutoSnakePlayer<SS>,
 	speed: u32,
+	autoplay: bool,
+	overlay: bool,
 }
 
 impl<SS: SnakeSolver> MyApp<SS> {
@@ -35,6 +37,8 @@ impl<SS: SnakeSolver> MyApp<SS> {
 		Self {
 			world: AutoSnakePlayer::new(GRID_SIZE as usize, solver),
 			speed: 1,
+			autoplay: false,
+			overlay: true,
 		}
 	}
 }
@@ -45,9 +49,11 @@ impl<SS: SnakeSolver> eframe::App for MyApp<SS> {
 			match self.world.state() {
 				AutoPlayerState::Playing | AutoPlayerState::Killed => {
 					let mut widget = SnakeWorldViewer::new(&self.world.world());
-					widget = widget.with_path_overlay(self.world.current_path());
 
-					widget = self.world.solver.decorate_widget(widget);
+					if self.overlay {
+						widget = widget.with_path_overlay(self.world.current_path());
+						widget = self.world.solver.decorate_widget(widget);
+					}
 
 					ui.add(widget);
 				}
@@ -59,36 +65,34 @@ impl<SS: SnakeSolver> eframe::App for MyApp<SS> {
 			ui.horizontal(|ui| {
 				ui.label("Speed");
 				ui.add(egui::Slider::new(&mut self.speed, 1..=10000));
+				ui.add(egui::Checkbox::new(&mut self.autoplay, "Autoplay"));
+				ui.add(egui::Checkbox::new(&mut self.overlay, "Overlay"));
 			});
 
-			if ctx.input().key_pressed(egui::Key::Space) {
-				for _ in 0..self.speed {
-					self.world.step();
-				}
+			let mut steps = 0;
+
+			if self.autoplay || ctx.input().key_pressed(egui::Key::Space) {
+				steps = self.speed as usize;
 			}
 
 			if ctx.input().key_pressed(egui::Key::Num1) {
-				for _ in 0..(self.speed) {
-					self.world.step();
-				}
+				steps = self.speed as usize;
 			}
 
 			if ctx.input().key_pressed(egui::Key::Num2) {
-				for _ in 0..(self.speed * 10) {
-					self.world.step();
-				}
+				steps = self.speed as usize * 10;
 			}
 
 			if ctx.input().key_pressed(egui::Key::Num3) {
-				for _ in 0..(self.speed * 100) {
-					self.world.step();
-				}
+				steps = self.speed as usize * 100;
 			}
 
 			if ctx.input().key_pressed(egui::Key::Num4) {
-				for _ in 0..(self.speed * 1000) {
-					self.world.step();
-				}
+				steps = self.speed as usize * 1000;
+			}
+
+			for _ in 0..steps {
+				self.world.step();
 			}
 
 			ctx.request_repaint();
